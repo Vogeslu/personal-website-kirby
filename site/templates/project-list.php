34357534288943651c686110a9a339846f4d9821
Luca Voges <vogeslu+git@gmail.com>
2024-07-21 10:58:56 +0200

    [ADD] Added toggle image cover and fixed short description

--- site/templates/project-list.php
+++ site/templates/project-list.php
@@ -30,16 +30,18 @@ foreach ($projects as $project) {
 
     <div class="grid grid-cols-2 gap-2 mb-8 lg:grid-cols-1">
         <?php foreach ($projects as $project) :
-            $previewImage = $project->previewImages()?->toStructure()?->first()?->image()->toFile();
+            $previewImage = $project->showImageCover()->toBool() ? $project->previewImages()?->toStructure()?->first()?->image()->toFile() : null;
         ?>
-            <div hx-get="<?= $project->url(['params' => ['preview' => true]]) ?>" hx-target="#project-preview" hx-swap="innerHTML" <?= attr(['class' => ['bg-slate-100 dark:bg-slate-800 border border-zinc-200 dark:border-zinc-700 rounded-lg cursor-pointer overflow-hidden relative hover:-translate-y-2 transition-transform', $previewImage != null ? 'pt-16' : '']]) ?>>
+            <div hx-get="<?= $project->url(['params' => ['preview' => true]]) ?>" hx-target="#project-preview" hx-swap="innerHTML" <?= attr(['class' => ['bg-slate-100 dark:bg-slate-800 border border-zinc-200 dark:border-zinc-700 rounded-lg cursor-pointer overflow-hidden relative hover:-translate-y-2 transition-transform flex flex-col', $previewImage != null ? 'pt-16' : '']]) ?>>
 
                 <?php if ($previewImage != null) : ?>
                     <div class="image h-52 bg-cover bg-center absolute left-0 top-0 w-full" style="background-image: url('<?= $previewImage->url() ?>')"></div>
                 <?php endif; ?>
-                <div <?= attr(['class' => ['p-4 relative z-10 bg-gradient-to-b from-transparent to-slate-100 dark:to-slate-800 to-50%', ...($previewImage != null ? ['pt-32'] : [])]]) ?>>
-                    <b class="block text-lg mb-4"><?= $project->title() ?></b>
-                    <p><?= $project->shortDescription()->kt() ?></p>
+                <div <?= attr(['class' => ['p-4 relative z-10 bg-gradient-to-b from-transparent to-slate-100 dark:to-slate-800 to-50% flex flex-col flex-1', ...($previewImage != null ? ['pt-32'] : [])]]) ?>>
+                    <b class="flex text-lg mb-4 flex-1 items-end"><?= $project->title() ?></b>
+                    <?php if ($project->shortDescription()->isNotEmpty()) : ?>
+                        <div class="mb-4"><?= $project->shortDescription()->kt() ?></div>
+                    <?php endif; ?>
                     <div class="flex gap-2 flex-wrap text-sm">
                         <?php if ($project->timespan()->isNotEmpty()) : ?>
                             <span class="font-semibold inline-block mr-2"><?= $project->timespan() ?></span>
