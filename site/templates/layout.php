e96a4bd1625ac6fbad46abb2e386cf96bac1ba2f
vogeslu <vogeslu@gmail.com>
2024-07-21 11:51:43 +0000

    [ADD] Added blocks to layout

--- site/templates/layout.php
+++ site/templates/layout.php
@@ -8,5 +8,18 @@
 
 snippet('layout', slots: true); ?>
 <h1 class="text-3xl font-bold mb-4"><?= $page->title() ?></h1>
-<?= $page->blocks()->toBlocks() ?>
-<?php endsnippet() ?>
+<?php
+$blocks = $page->blocks()->toBlocks();
+
+if ($blocks->count() > 0) :
+?>
+    <div class="content flex flex-col gap-6 flex-1">
+        <?php foreach ($page->blocks()->toBlocks() as $block) : ?>
+            <div id="<?= $block->id() ?>" class="block block-type-<?= $block->type() ?>">
+                <?php snippet('blocks/' . $block->type(), [
+                    'block' => $block
+                ]) ?>
+            </div>
+        <?php endforeach; ?>
+    </div>
+<?php endif; endsnippet() ?>
